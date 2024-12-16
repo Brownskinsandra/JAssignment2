d9eaa81fd88ae1fb348a5d09d6afad56fb1a3ee3
Sanny_okafor <okaforsan77@gmail.com>
2024-12-16 18:43:25 +0100

    Tailwind mastery

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'rectangle': "url('/pic/rectangle.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
